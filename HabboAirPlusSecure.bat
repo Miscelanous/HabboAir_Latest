684d2d391ba9523a7ff5ca09366962966c0ee7fd
Miscelanous <119067705+Miscelanous@users.noreply.github.com>
2022-11-30 06:02:05 -0300

    Delete HabboAirPlusSecure.bat

--- HabboAirPlusSecure.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Espera un momento mientras se descarga: HabboAIR+ Oficial
-title Descargando: HabboAIR+ Oficial
-powershell Command "(New-Object Net.WebClient).DownloadFile('https://github.com/LilithRainbows/HabboAirPlus/raw/main/HabboAir.swf', 'HabboAir.swf')"
-cls
-echo La descarga se ha completado.
-pause
